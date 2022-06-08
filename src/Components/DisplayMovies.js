8b02067064f1b74064a4bbb8659a099d8b6101af
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-06-08 14:45:06 -0600

    Connected Movie API service
    The DisplayMovies component calls the API service getMovieList, and returns the movie list object

--- src/Components/DisplayMovies.js
+++ src/Components/DisplayMovies.js
@@ -3,8 +3,26 @@ import { useNavigate, Link } from 'react-router-dom';
 import API from '../Services/API';
 
 const DisplayMovies = () => {
+  //declare states
+
+
+  useEffect(() => {
+    //API call here
+    API.getMovieList().then(res => {
+      console.log("received");
+    });
+  });
+
+  //update movie list function
+
+  //handle searchChange
+
+  //handle filterSelect
+
   return (
-    <div>test</div>
+    <div>test
+
+    </div>
   )
 };
 
---
+++ src/Services/API.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import React from 'react';
+
+
+const baseURL = "https://code-challenge.spectrumtoolbox.com/api/movies";
+const headers = { Authorization: { 'Api-Key': 'q3MNxtfep8Gt' }, };
+const config = ({
+  headers: {
+    Authorization: 'Api-Key q3MNxtfep8Gt',
+  },
+});
+
+const API = {
+  getMovieList: () => {
+    return axios.get(`${baseURL}`, config);
+  },
+
+  getMovieDetails: (id) => {
+    return axios.get(`${baseURL}`, config);
+  }
+};
+
+
+export default API;

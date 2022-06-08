130e5090b3028a28a437abcc4b47eb270f4d4586
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-06-08 13:56:46 -0600

    Basic skeleton built and connected
    I filled in the App.js with some basic navigation code for the needed routes (will add more later).  Created the first Component DisplayMovies and also created the file for our API service

---
+++ src/Components/DisplayMovies.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import API from '../Services/API';
+
+const DisplayMovies = () => {
+  return (
+    <div>test</div>
+  )
+};
+
+export default DisplayMovies;

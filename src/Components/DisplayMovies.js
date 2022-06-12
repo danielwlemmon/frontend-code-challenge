3f821949a51697e4e8d8354aa01827128171d199
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-06-12 14:02:40 -0600

    Genre List pulled and set
    pulling Genres from Movie data, and setting it to the genreList state.  Displaying the list of possible genres in a dropdown menu along with the default value of "All";

--- src/Components/DisplayMovies.js
+++ src/Components/DisplayMovies.js
@@ -1,28 +1,32 @@
+import { render } from '@testing-library/react';
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import API from '../Services/API';
 
 const DisplayMovies = () => {
   const [filteredMovieList, setFilteredMovieList] = useState([]);
-  const [searchText, setSearchText] = useState("");
   const [fullMovieList, setFullMovieList] = useState([]);
-
-  //will change to pulling genre names from data object
-  const [genres, setGenres] = useState(
-    ["Action", "Thriller", "Crime drama", "Mystery", "Children", "Comedy",
-      "Adventure", "Animated", "Drama", "Comedy Drama", "Western", "Historical drama"]
-  );
+  const [genreList, setGenreList] = useState([]);
+  const [genreFilter, setGenreFilter] = useState("All");
 
   useEffect(() => {
     API.getMovieList().then((res) => {
       if (res.data.message.toLowerCase() === "success") {
         setFilteredMovieList(res.data.data);
         setFullMovieList(res.data.data);
+        let genreArr = ["All"];
+        for (let i = 0; i < res.data.data.length; i++) {
+          genreArr.push(res.data.data[i].genres);
+        };
+        genreArr = genreArr.flat();
+        // setTimeout(() => {
+
+        setGenreList([...new Set(genreArr)]);
+        // }, "300")
       } else {
         console.log(res.data.message);
       };
     });
-
   }, []);
 
   const checkImg = (id) => {
@@ -33,6 +37,12 @@ const DisplayMovies = () => {
     }
   };
 
+  // const filterByGenre = (e) => {
+  //   e.preventDefault();
+
+  //   console.log(e.target.value);
+  // }
+
   const handleSearch = (e) => {
     e.preventDefault();
     var lowerCase = (e.target.value.toLowerCase());
@@ -42,32 +52,30 @@ const DisplayMovies = () => {
       return data.title.toLowerCase().search(lowerCase) != -1;
     });
     setFilteredMovieList(result);
+
   };
-
-  const checkNoResults = () => {
-    if (filteredMovieList.length == 0) {
-      return "No results found...";
-    }
-  }
-
-
 
   return (
     <div>
-      <label name="search">Search movie list  </label>
-      <input label="search" onChange={handleSearch}></input>
-
-      <div>
-        {filteredMovieList.length > 0 ? filteredMovieList?.map((movie) => (
-
-          <div key={movie.id}>
-            <Link to={`/movie-details/${movie.id}`}>
-              {checkImg(movie.id)}
-            </Link>
-          </div>
-        )): <p>No results found...</p>}
+      <div className='search-bar'>
+        <label className='search-label' name="search">Search movie list  </label>
+        <input label="search" onChange={handleSearch}></input>
+        <select>
+          {genreList.length > 1 ? genreList?.map((genre, idx) => (
+            <option key={idx}>{genre}</option>
+          )) : <option>Loading Genres...</option>}
+        </select>
       </div>
-
+      <div className='movies'>
+        {filteredMovieList.length > 0 ? filteredMovieList?.map((movie) => (
+          <div key={movie.id} className="one_movie">
+            <h5 className='movie-title'>{movie.title}</h5>
+            <a href={`/movie-details/${movie.id}`}>
+              {checkImg(movie.id)}
+            </a>
+          </div>
+        )) : <p>No results found...</p>}
+      </div>
     </div>
   )
 };
